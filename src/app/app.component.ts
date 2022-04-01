1f6d07f4228a5d6aea1e104a4288ea6d89e41ad8
Maghawry <mag.hauwry@gmail.com>
2022-04-01 13:34:54 +0200

    App now generates a random number based on the inputs of the user.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,9 +33,27 @@ export class AppComponent {
   }
 
   onButtonClick(){
-    console.log(this.includeLetters, this.includeNumbers, 
-      this.includeSymbols, this.length)
-    this.password = 'My password!!!'
-  }
+    const numbers = '1234567890';
+    const letters = 'qwertyuiopasdfghjklzxcvbnm';
+    const symbols = '!@#$%^&*()';
+    let validChars = '';
 
+    if(this.includeLetters){
+      validChars += letters;
+    }
+    if(this.includeNumbers){
+      validChars += numbers;
+    }
+    if(this.includeSymbols){
+      validChars += symbols;
+    }
+
+    let generatedPassword = ''
+
+    for (let i = 0; i < this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
 }
